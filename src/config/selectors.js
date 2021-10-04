e6534f4ae9eae1a37d906d8a5fefcdb84a770d53
Juan Carlos Farah <juancarlos.farah@pm.me>
2021-10-04 10:10:04 +0200

    feat: allow code to be dynamically set

---
+++ src/config/selectors.js
@@ -0,0 +1,3 @@
+export const HEADER_VISIBILITY_SWITCH_CYPRESS = 'headerVisibility';
+export const SETTINGS_MODAL_CYPRESS = 'settingsModal';
+export const SAVE_SETTINGS_BUTTON_CYPRESS = 'saveSettingsButton';
